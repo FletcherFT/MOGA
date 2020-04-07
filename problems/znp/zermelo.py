efad6407b714276d89f7287a990e64ea796a1981
FletcherFT <fletcherthompsonx@gmail.com>
2020-04-07 07:44:06 +0200

    Tested atexit (no success). Added constraint fitness function. Added compatibility for using constraints.

--- problems/znp/zermelo.py
+++ problems/znp/zermelo.py
@@ -6,8 +6,13 @@ from numpy.matlib import repmat
 
 from mogapy import ndsa1, utils
 
+import atexit
+
 np.random.seed(42)
 
+@atexit.register
+def goodbye():
+    print("You are now leaving the Python sector.")
 
 def constant_V(x, y, c, theta):
     V = np.ones((x.shape[0], x.shape[1], 2))
@@ -21,10 +26,12 @@ def linear_shear_V(x, y, m, c):
     V[:, :, 0] = m * y + c
     return V
 
+
 def cubic_shear_V(x, y, m, c):
     V = np.zeros((x.shape[0], x.shape[1], 2))
-    V[:, :, 0] = m * y**3 + c
+    V[:, :, 0] = m * y ** 3 + c
     return V
+
 
 def fun_shear_V(x, y, cx, cy):
     V = np.zeros((x.shape[0], x.shape[1], 2))
@@ -33,7 +40,35 @@ def fun_shear_V(x, y, cx, cy):
     return V
 
 
-def fitness(S, V, **kwargs):
+def constraint(S, V, maxw=0, **kwargs):
+    """Constraint Fitness Calculation
+    Given S, V
+    1. Calculate w
+    2. Calculate maximum speed violation."""
+    # S'
+    S_prime = np.gradient(S, axis=1)
+    # Get the number, length and Dimension of solutions
+    N, L, Dim = S.shape
+    # Break out the X and Y coordinates of S
+    X = S[:, :, 0].flatten()
+    Y = S[:, :, 1].flatten()
+    # Break out the u and v components of V
+    u = V[:, :, 0]
+    v = V[:, :, 1]
+    u_s = u[Y, X].reshape((N, L, 1))
+    v_s = v[Y, X].reshape((N, L, 1))
+    # V(s)
+    V_s = np.concatenate((u_s, v_s), axis=-1)
+    # w
+    w = S_prime - V_s
+    # Speed magnitude
+    w_mag = np.sqrt((w ** 2).sum(axis=2))
+    # Constraint violation score
+    c = np.where(maxw - w_mag < 0, w_mag - maxw, 0).sum(axis=1).reshape((N, 1))
+    return c
+
+
+def fitness(S, V, dt=1, **kwargs):
     """Fitness Calculation
     Given S, V
     1. Calculate S'
@@ -57,8 +92,6 @@ def fitness(S, V, **kwargs):
     V_s = np.concatenate((u_s, v_s), axis=-1)
     # w
     w = S_prime - V_s
-    # Speed overstepping
-    w_mag = ((np.sqrt((w**2).sum(axis=2))) > 5).sum(axis=1).reshape((N,1))
     # curve integral of all solutions in S
     D = np.sqrt((np.diff(S, axis=1) ** 2).sum(axis=-1)).sum(axis=-1).reshape((N, 1))
     # energy integral of all solutions in w
@@ -83,15 +116,15 @@ if __name__ == "__main__":
     # GRID is a list, element 0 is the x coordinate of a tile. Element 1 is the y coordinate of a tile.
     GRID_X, GRID_Y = np.meshgrid(X, Y)
     # The velocity vector field, comprised of u and v components
-    #V = linear_shear_V(GRID_X, GRID_Y, 3, 0)
-    #V = constant_V(GRID_X, GRID_Y, 1, -np.pi/4)
-    #V = cubic_shear_V(GRID_X, GRID_Y, 1, 0)
-    V = fun_shear_V(GRID_X, GRID_Y, 1, 0)
+    #V = linear_shear_V(GRID_X, GRID_Y, 1, 0)
+    V = constant_V(GRID_X, GRID_Y, 1, -np.pi/4)
+    # V = cubic_shear_V(GRID_X, GRID_Y, 1, 0)
+    # V = fun_shear_V(GRID_X, GRID_Y, 1, 0)
     # Vector Field
     COST = np.concatenate((np.expand_dims(GRID_X, 2), np.expand_dims(GRID_Y, 2), V), axis=2)
     # Number of solutions
     N = 50
-    # Pareto Front Length
+    # Pareto Front Length (Need to keep equal to N for now).
     NP = N
     # Initialise the solutions array, alleles for the moment are just random elements in X
     chromosomes = np.stack((np.random.randint(0, WIDTH, (N, DEPTH), np.int),
@@ -112,48 +145,53 @@ if __name__ == "__main__":
     # Enforce finishing X condition
     chromosomes[:, -1, 0] = FINISH
     # The Solver Class
-    solver = ndsa1.NDSA1(N, fitness=fitness)
+    #solver = ndsa1.NDSA1(N, fitness=fitness)
+    solver = ndsa1.NDSA1(N, fitness=fitness, constraint=constraint)
+    maxw = np.inf
     # The Logger Class
     outfile = Path("./results").resolve()
     outfile.mkdir(parents=True, exist_ok=True)
     f = list(outfile.glob("log_run*"))
     outfile = outfile.joinpath("log_run_{:03d}.mp4".format(len(f) + 1))
     # If you want a video logged, then pass FFMpegWriter keyword arguments and an outfile keyword for the video output path.
-    #logger = utils.ResultsManager(fps=60, outfile=str(outfile))
+    logger = utils.ResultsManager(fps=30, outfile=str(outfile))
     # If you don't want a video logged, then don't pass arguments to ResultsManager
-    logger = utils.ResultsManager()
+    #logger = utils.ResultsManager()
     # Solution Display
     outfile = Path("./results").resolve()
     outfile.mkdir(parents=True, exist_ok=True)
     f = list(outfile.glob("sol_run*"))
     outfile = outfile.joinpath("sol_run_{:03d}.mp4".format(len(f) + 1))
     # If you want a video logged, then pass FFMpegWriter keyword arguments and an outfile keyword for the video output path.
-    #sol = utils.ResultPlotter(NP, COST, fps=60, outfile=str(outfile))
+    sol = utils.ResultPlotter(NP, COST, fps=30, outfile=str(outfile))
     # If you don't want a video logged, then don't pass arguments to ResultPlotter
-    sol = utils.ResultPlotter(NP, COST)
+    #sol = utils.ResultPlotter(NP, COST)
     # Number of generations
     GBEST = None
-    for i in range(3000):
-        chromosomes, fitnesses = solver.update(chromosomes, V=V, bounds=bounds, lineq=(A, b))
+    for i in range(1000):
+        # Get the next generation of chromosomes, their fitnesses and constraints
+        chromosomes, fitnesses, constraints = solver.update(chromosomes, V=V, bounds=bounds, lineq=(A, b), maxw=maxw)
+        # If the GBEST population hasn't been initialised
         if GBEST is None:
-            rankings = ndsa1.ndsa(fitnesses)
+            # Get the best performers
+            rankings = ndsa1.cndsa2(fitnesses, constraints)
+            # Update the archives solution, fitness and constraints
             GBEST = chromosomes[rankings[0], :, :]
             GFBEST = fitnesses[rankings[0], :]
+            GCBEST = constraints[rankings[0], :]
+        # Otherwise, perform survival on the archive and latest batch
         else:
             tmpS = np.vstack((GBEST, chromosomes))
             tmpF = np.vstack((GFBEST, fitnesses))
-            rankings = ndsa1.ndsa(tmpF)
-            GBEST = tmpS[rankings[0], :, :]
-            GFBEST = tmpF[rankings[0], :]
-            if GBEST.shape[0] > NP:
-                d = ndsa1.crowding_distance([[0 for _ in range(GBEST.shape[0])]], GFBEST)
-                sur_idx = np.flipud(np.argsort(d))
-                sur_idx = sur_idx[:NP]
-                GBEST = GBEST[sur_idx, :, :]
-                GFBEST = GFBEST[sur_idx, :]
-        print("Iteration {:04d}\t GBEST Size: {:03d}".format(i + 1, GBEST.shape[0]))
+            tmpC = np.vstack((GCBEST, constraints))
+            rankings = ndsa1.cndsa2(tmpF, tmpC)
+            sur_idx = solver._survival(tmpS, rankings, tmpF)
+            GBEST = tmpS[sur_idx, :, :]
+            GFBEST = tmpF[sur_idx, :]
+            GCBEST = tmpC[sur_idx, :]
+        print("Iteration {:04d}\tGBEST Size {:03d}".format(i + 1, GBEST.shape[0]))
         sol.update(GBEST)
-        logger.update(GFBEST, ["Distance", "Energy"], linestyle="None", marker=".", markersize=10, color="green")
+        logger.update(np.hstack((GFBEST,GCBEST)), ["Distance", "Energy", "Speed Violation"], linestyle="None", marker=".", markersize=10, color="green")
     if sol.flag:
         sol.finish()
     if logger.flag:
